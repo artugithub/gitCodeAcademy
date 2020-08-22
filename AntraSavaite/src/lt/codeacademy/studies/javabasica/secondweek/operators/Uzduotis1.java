2ffc07fe9eec4bee7e08ee403421d685ec9f037a
artugithub <artugithub@gmail.com>
2020-08-22 20:03:27 +0300

    Added user input, 1st string output.

--- AntraSavaite/src/lt/codeacademy/studies/javabasica/secondweek/operators/Uzduotis1.java
+++ AntraSavaite/src/lt/codeacademy/studies/javabasica/secondweek/operators/Uzduotis1.java
@@ -1,11 +1,13 @@
 package lt.codeacademy.studies.javabasica.secondweek.operators;
 
+import java.util.Scanner;
+
 public class Uzduotis1 {
 
 	public static void main(String[] args) {
 		String sakinys = ivesti();
 		int indeksas1 = 0;
-		int indeksas2 = sakinys.length(); // pakeisti
+		int indeksas2 = sakinys.indexOf(" "); // pakeisti
 
 		String z1 = sakinys.substring(indeksas1, indeksas2);
 
@@ -14,7 +16,9 @@ public class Uzduotis1 {
 
 	private static String ivesti() {
 		// TODO Auto-generated method stub
-		return null;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Iveskite sakini is keliu zodziu");
+		return sc.nextLine();
 	}
 
 }
